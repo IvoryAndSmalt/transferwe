7c3ed00670b799276c66922c610b7c6474f97478
Lucas <lucas180395@gmail.com>
2019-01-07 09:59:05 +0100

    ajout bdd

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -2,23 +2,4 @@
 
 include('Models/Home.php');
 
-if(isset($_POST['oui'])){
-    getAllRepo();
-}
-if(isset($_POST['non'])){
-    getAllIDs();
-    getOneProject(2);
-    getOneProject(2);
-    getOneProject(2);
-    getOneProject(2);
-    getOneProject(2);
-    getOneProject(2);
-    getOneProject(2);
-    getOneProject(2);
-    getOneProject(2);
-    getOneProject(2);
-}
-
 include('Views/HomeView.php');
-
-?>
--- Models/Home.php
+++ Models/Home.php
@@ -1,17 +1,6 @@
 <?php
 
-// include('info.php');
+include('info.php');
 
-// $dbh = new PDO('mysql:host='. $host .';dbname='. $dbname, $user, $pass);
-// $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
-
-function getAllRepo(){
-    global $dbh;
-
-    $all_repo = $dbh->prepare('SELECT * FROM projets');
-    $all_repo->execute();
-
-    $all_repo = $all_repo->fetchAll();
-
-    return $all_repo;
-}
+	$dbh = new PDO('mysql:host='. $host .';dbname='. $dbname, $user, $pass);
+	$dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
