d0ef79c692202a2a82b2337e8bc0a35ebb54d762
Lucas <lucas180395@gmail.com>
2019-01-07 09:34:49 +0100

    ajout exinfo à renommer en info.php

---
+++ exinfo.php
@@ -0,0 +1,9 @@
+<?php
+//Renommez ce fichier en info.php, avec vos informations personnelles.
+
+$host = "localhost";
+$dbname = "dbname;charset=utf8";
+$user = "root";
+$pass = "";
+
+$base_url = "localhost/transferwe/";
