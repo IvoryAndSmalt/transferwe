5caea05e461675cef820d457b1219f67dfb45a70
Neithy88 <alexandra.moullet@gmail.com>
2019-01-08 09:25:44 +0100

    comit 08.01 9h25

--- Views/HomeView.php
+++ Views/HomeView.php
@@ -6,171 +6,41 @@ include 'header.php';
 
 ?>
 
+<link rel="stylesheet" href="assets/css/HomeView.css">
 
-<style>
-        @import url(https://fonts.googleapis.com/css?family=Roboto:400,300,600,400italic);
-            * {
-              margin: 0;
-              padding: 0;
-              box-sizing: border-box;
-              -webkit-box-sizing: border-box;
-              -moz-box-sizing: border-box;
-              -webkit-font-smoothing: antialiased;
-              -moz-font-smoothing: antialiased;
-              -o-font-smoothing: antialiased;
-              font-smoothing: antialiased;
-              text-rendering: optimizeLegibility;
-            }
-            
-            body {
-              font-family: "Roboto", Helvetica, Arial, sans-serif;
-              font-weight: 100;
-              font-size: 12px;
-              line-height: 30px;
-              color: #777;
-              background: #4CAF50;
-            }
-            
-            .container {
-              max-width: 400px;
-              width: 100%;
-              margin: 0 auto;
-              position: relative;
-            }
-            
-            #contact input[type="text"],
-            #contact input[type="email"],
-            #contact input[type="tel"],
-            #contact input[type="url"],
-            #contact textarea,
-            #contact button[type="submit"] {
-              font: 400 12px/16px "Roboto", Helvetica, Arial, sans-serif;
-            }
-            
-            #contact {
-              background: #F9F9F9;
-              padding: 25px;
-              margin: 150px 0;
-              box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
-            }
-            
-            #contact h3 {
-              display: block;
-              font-size: 30px;
-              font-weight: 300;
-              margin-bottom: 10px;
-            }
-            
-            #contact h4 {
-              margin: 5px 0 15px;
-              display: block;
-              font-size: 13px;
-              font-weight: 400;
-            }
-            
-            fieldset {
-              border: medium none !important;
-              margin: 0 0 10px;
-              min-width: 100%;
-              padding: 0;
-              width: 100%;
-            }
-            
-            #contact input[type="text"],
-            #contact input[type="email"],
-            #contact input[type="tel"],
-            #contact input[type="url"],
-            #contact textarea {
-              width: 100%;
-              border: 1px solid #ccc;
-              background: #FFF;
-              margin: 0 0 5px;
-              padding: 10px;
-            }
-            
-            #contact input[type="text"]:hover,
-            #contact input[type="email"]:hover,
-            #contact input[type="tel"]:hover,
-            #contact input[type="url"]:hover,
-            #contact textarea:hover {
-              -webkit-transition: border-color 0.3s ease-in-out;
-              -moz-transition: border-color 0.3s ease-in-out;
-              transition: border-color 0.3s ease-in-out;
-              border: 1px solid #aaa;
-            }
-            
-            #contact textarea {
-              height: 100px;
-              max-width: 100%;
-              resize: none;
-            }
-            
-            #contact button[type="submit"] {
-              cursor: pointer;
-              width: 100%;
-              border: none;
-              background: #4CAF50;
-              color: #FFF;
-              margin: 0 0 5px;
-              padding: 10px;
-              font-size: 15px;
-            }
-            
-            #contact button[type="submit"]:hover {
-              background: #43A047;
-              -webkit-transition: background 0.3s ease-in-out;
-              -moz-transition: background 0.3s ease-in-out;
-              transition: background-color 0.3s ease-in-out;
-            }
-            
-            #contact button[type="submit"]:active {
-              box-shadow: inset 0 1px 3px rgba(0, 0, 0, 0.5);
-            }
-            
-            .copyright {
-              text-align: center;
-            }
-            
-            #contact input:focus,
-            #contact textarea:focus {
-              outline: 0;
-              border: 1px solid #aaa;
-            }
-            
-            ::-webkit-input-placeholder {
-              color: #888;
-            }
-            
-            :-moz-placeholder {
-              color: #888;
-            }
-            
-            ::-moz-placeholder {
-              color: #888;
-            }
-            
-            :-ms-input-placeholder {
-              color: #888;
-            }
-
-            .errorMsg {
-                color:red;
-            }
-
-            .errorMessage {
-                color:red;
-                display: none;
-            }
-    </style>
+<link href="assets/css/bootstrap.css" rel="stylesheet" />
+<link href="assets/css/rotating-card.css" rel="stylesheet" />
+<link href="https://netdna.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet" />
 
 </head>
 
 <body>
 
-    <div class="container">
-        <form id="contact" action="page.php" method="post">
-            <h3>Nom</h3>
-            <h4>Ajoutez vos fichiers</h4>
+<div class="card-container">
+         <div class="card">
+             <div class="front">
+                 <div class="user">
+                     <img class="img" src="assets/img/plume.png"/>
+                 </div>
+                 <div class="content">
+                     <div class="main">
+                         <h3 class="name">Inna Corman</h3>
+                         <p class="profession">Product Manager</p>
+
+                         <p class="text-center">"I'm the new Sinatra, and since I made it here I can make it anywhere, yeah, they love me everywhere"</p>
+                     </div>
+                     <div class="footer">
+                         <div class="rating">
+                             <i class="fa fa-mail-forward"></i> Partager votre fichier
+                         </div>
+                     </div>
+                 </div>
+            </div> <!-- end front panel -->
+             <div class="back">
+                 <div class="content">
+                     <div class="main">
+                     <form id="contact" action="page.php" method="post">
+            <h3>Partager vos fichiers</h3>
             <input type="file" name="icone" id="icone" /><br />
                 <label for="mon_fichier">Fichier (tous formats | max. 1 Mo) :</label><br />
                 <input type="hidden" name="MAX_FILE_SIZE" value="1048576" />
@@ -203,6 +73,19 @@ include 'header.php';
                 <button name="submitBtn" type="submit" id="contact-submit" data-submit="...Sending">Transférer</button>
             </fieldset>
         </form>
+
+                
+
+                     </div>
+                 </div>
+                 
+             </div> <!-- end back panel -->
+         </div> <!-- end card -->
+     </div> <!-- end card-container -->
+
+
+
+       
     </div>
 
 
--- Views/header.php
+++ Views/header.php
@@ -7,7 +7,6 @@
     <title><?=$title?> - Lucas van den Berg</title>
     <link rel="manifest" href="manifest.json">
     <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="shortcut icon" href="assets/img/logo.svg" type="image/x-icon">
 </head>
 <body id="body" class="bg-smalt">
 
@@ -18,48 +17,9 @@
         <ul class="bannerul">
             <li class="headerli thin">
                 <p>
-                    <a href="#main"><img id="logo" src="assets/img/logo.svg" alt="Logo"></a>
+                    <a href="#main"><img id="logo" src="assets/img/logo.png" alt="Logo"></a>
                 </p>
             </li>
-            <li class="headerli thin lgmenus" id="titleHome">
-                <p>
-                    <a href="#main">
-                        <span class="french">Accueil</span>
-                        <span class="english">Home</span>
-                    </a>
-                </p>
-            </li>
-            <li class="headerli thin lgmenus" id="titleCV">
-                <p>
-                    <a href="#main">
-                        <span>CV</span>
-                    </a>
-                </p>
-            </li>
-            <li class="headerli thin lgmenus" id="titleProjects">
-                <p>
-                    <a href="#main">
-                        <span class="french">Projets</span>
-                        <span class="english">Projects</span>
-                    </a>
-                </p>
-            </li>
-            <li class="headerli thin lgmenus" id="titleContact">
-                <a href="#contactform">
-                    <p>
-                        <span>Contact</span>
-                    </p>
-                </a>
-            </li>
-            <li class="headerli thin">
-                <ul class="flags">
-                    <li class="flag english" id="switchFrench" title="switch to French">
-                        <img src="assets/img/france.svg" alt="Drapeau Français">
-                    </li>
-                    <li class="flag french" id="switchEnglish" title="passer à l'anglais">
-                        <img src="assets/img/united-kingdom.svg" alt="Drapeau Anglais">
-                    </li>
-                </ul>
-            </li>
+            
         </ul>
     </header>
