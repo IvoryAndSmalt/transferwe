843bca6822957c54680a3e80e8edef00fbca0714
Lucas <lucas180395@gmail.com>
2019-01-08 13:56:46 +0100

    oui

--- Views/header.php
+++ Views/header.php
@@ -4,22 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?=$title?> - Lucas van den Berg</title>
+    <title><?=$title?></title>
     <link rel="manifest" href="manifest.json">
     <link rel="stylesheet" href="assets/css/style.css">
+    <!-- <link rel="shortcut icon" href="assets/img/logo.svg" type="image/x-icon"> -->
 </head>
-<body id="body" class="bg-smalt">
+<body>
 
-<!-- Behold, the field in which I grow my PHP. Lay thine eyes upon it and thou shalt see that it is barren.  
-        A.K.A. navigation in pure JS, because I can. -->
+    <header id="header" class="">
 
-    <header id="header" class="bg-ivory">
-        <ul class="bannerul">
-            <li class="headerli thin">
-                <p>
-                    <a href="#main"><img id="logo" src="assets/img/logo.png" alt="Logo"></a>
-                </p>
-            </li>
-            
-        </ul>
     </header>
