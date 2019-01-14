6a9f8158fba35ed5d63b8d734c5640bf26721d38
Morgannec <morganne.culas@hotmail.fr>
2019-01-14 14:33:27 +0100

    nfbejqs

--- Views/EnvoiView.php
+++ Views/EnvoiView.php
@@ -11,7 +11,7 @@ include 'header.php';
 <div class="container">
     <div class="row">
     <h1 class="text-center" id="titredl">Votre fichier a bien été envoyé</h1>
-    <a href="<?=$lien?>" title="<?=$nom?>"class="text-center"><img src="assets/img/file-upload-o.png" alt="download your file" id="file" ></a> 
+    <a id = "envoiimg" href="<?=$lien?>" title="<?=$nom?>"class="text-center"><img src="assets/img/file-upload-o.png" alt="download your file" id="file" ></a> 
     <h4 class="text-center">Télécharger votre fichier ici !</h4>
     </div>
     
