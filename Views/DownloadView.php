1c746c9e21d93c60c7af59b6d310d097c4589a84
Lucas <lucas180395@gmail.com>
2019-01-14 11:13:31 +0100

    suppression

--- Views/DownloadView.php
+++ Views/DownloadView.php
@@ -4,11 +4,8 @@ $title = "TransferWe : Download";
 include 'header.php';
 
 ?>
-</head>
-
 
 <a href="Home" ><img id="logo" src="assets/img/logo.png" alt="Logo"></a>
-  
 
 <body>
 
@@ -20,40 +17,6 @@ include 'header.php';
     </div>
     
 </div>
-
-
-    <!-- <div class="container">
-        <div class="row">
-            <div class="col-sm-10 col-sm-offest-1">
-                <div class="card-container manual-flip">
-                    <div class="card">
-                        <div class="front">
-                            <div class="user">
-                                <img class="img-circle" src="assets/img/plume.png"/>
-                            </div>
-                            <div class="content">
-                                <div class="main">
-                                    <h3 class="name"></h3>
-                                    <p class="profession">Web Developer</p>
-                                    <p class="text-center">Tu as pleins de fichiers à envoyer ? N'hésites pas
-                                    une seconde ! <br>Clique juste en dessous ^^</p>
-                                </div>
-                                <div class="footer">
-                                <button class="btn btn-simple" onclick="rotateCard(this)">
-                                </i> <i class="fas fa-file-export"></i>Télécharger vos fichers
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-
-
-
-                </div>
-            </div>
-
-            </div>
-        </div>
-    </div> -->
 
 <?php
 include 'footer.php';
